80d22aed0348e8c6fbe79aeb3d0eb46197433334
pofkuma <pof.kuma@gmail.com>
2021-10-17 21:18:02 +0900

    Change constant to variable
    条件によって値が違うものは変数にする

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -6,21 +6,21 @@ require 'date'
 OPTIONS = ARGV.getopts("m:y:")
 
 DATE_TODAY = Date.today
-MON  = OPTIONS["m"] ? OPTIONS["m"].to_i : DATE_TODAY.mon
-YEAR = OPTIONS["y"] ? OPTIONS["y"].to_i : DATE_TODAY.year
+month = OPTIONS["m"] ? OPTIONS["m"].to_i : DATE_TODAY.mon
+year  = OPTIONS["y"] ? OPTIONS["y"].to_i : DATE_TODAY.year
 
 CAL_WIDTH = 20
-puts "#{MON}月 #{YEAR}".center(CAL_WIDTH)
+puts "#{month}月 #{year}".center(CAL_WIDTH)
 
 LINE_CWDAY = "日 月 火 水 木 金 土"
 puts LINE_CWDAY
 
 FIRST_DAY = 1
-LAST_DAY = Date.new(YEAR, MON, -1).day
+LAST_DAY = Date.new(year, month, -1).day
 days = (FIRST_DAY..LAST_DAY).to_a.map(&:to_s)
 
 LENGTH_A_WEEK = 7
-FIRST_CWDAY = Date.new(YEAR, MON).cwday
+FIRST_CWDAY = Date.new(year, month).cwday
 FIRST_CWDAY.times { days.unshift("\s") } if FIRST_CWDAY < LENGTH_A_WEEK
 
 days = days.map { |day| day.rjust(2) }
