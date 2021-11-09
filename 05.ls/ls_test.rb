2cd487f29236406beb8321e6692c6c0b6e6dd7a5
pofkuma <pof.kuma@gmail.com>
2021-11-09 14:34:24 +0900

    Modify test
    テストコードを修正

--- 05.ls/ls_test.rb
+++ 05.ls/ls_test.rb
@@ -11,44 +11,50 @@ class LsTest < Minitest::Test
   end
 
   def test_ls_no_options_one_contents
-    content_name_lists = [1]
+    content_name_lists = %w[1]
     expected = <<~TEXT
-      1
+      1       
     TEXT
     assert_equal expected, print_content_lists(content_name_lists)
   end
 
   def test_ls_no_options_two_contents
-    content_name_lists = %w[1 2]
+    content_name_lists = %w[1 2].shuffle
     expected = <<~TEXT
-      1       2
+      1       2       
     TEXT
     assert_equal expected, print_content_lists(content_name_lists)
   end
 
   def test_ls_no_options_three_contents
-    content_name_lists = %w[1 2 3]
+    content_name_lists = %w[1 2 3].shuffle
     expected = <<~TEXT
-      1       2       3
+      1       2       3       
     TEXT
     assert_equal expected, print_content_lists(content_name_lists)
   end
 
   def test_ls_no_options_four_contents
-    content_name_lists = %w[1 2 3 4]
+    content_name_lists = %w[1 2 3 4].shuffle
     expected = <<~TEXT
-      1       3
-      2       4
+      1       3       
+      2       4       
     TEXT
     assert_equal expected, print_content_lists(content_name_lists)
   end
 
   def test_ls_no_options_five_contents
-    content_name_lists = %w[1 2 3 4 5]
+    content_name_lists = %w[1 2 3 4 5].shuffle 
     expected = <<~TEXT
-      1       3       5
-      2       4
+      1       3       5       
+      2       4       
     TEXT
+    assert_equal expected, print_content_lists(content_name_lists)
+  end
+
+  def test_ls_no_options_contents_which_have_names_starting_with_dot
+    content_name_lists = %w[. ..].shuffle 
+    expected = ''
     assert_equal expected, print_content_lists(content_name_lists)
   end
 end
