48ba9d6ec0696e8ac4d5b2c05d65b58fca4725f4
pofkuma <pof.kuma@gmail.com>
2021-11-09 14:44:48 +0900

    Fix bug and Delete commment
    バグ修正と不要なコメントの削除

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -6,43 +6,35 @@ PADDING_SIZE = 7
 MAX_COLUMUNS = 3
 
 public
-def print_content_lists(content_name_lists)
-  content_name_lists.format_width.format_lines
-end
 
-# 所定のディレクトリ直下のファイル(ディレクトリを含む)を取得する: Dir.entries
-# ファイル名を昇順で並び替える: sort
-# ドットで始まるファイルsを除外する
-def fetch_target_content_names(directory_path)
-  lists = Dir.entries(directory_path).sort
-  lists.reject { |list| list =~ /^\./ }
+def print_content_lists(content_name_lists)
+  content_name_lists.reject { |i| /^\./.match?(i) }.sort.format_width.format_lines
 end
 
 # 表示幅を空白詰めで揃える
 def format_width
-  #   最長のファイル名の桁数を取得する: max_byとlength
   max_name_length = map(&:length).max
-  #   上の桁数＋余白の桁数で左詰する: mapとljust
   map { |list| list.ljust(max_name_length + PADDING_SIZE) }
 end
 
 # 折り返して表示する
 def format_lines
-  #   表示行数を求める
+  # 表示行数を求める
   rows = size / MAX_COLUMUNS
-  #      ファイル数を列数で割って、割り切れたら商の値、割り切れなかったら商+1の値
+  # ファイル数を列数で割って、割り切れたら商の値、割り切れなかったら商+1の値
   rows += 1 unless (size % MAX_COLUMUNS).zero?
-  return self if rows == 0
+  return '' if rows.zero?
 
-  #   表示行数で区切って、列数分だけ横にくっつける
+  # 表示行数で区切って、列数分だけ横にくっつける
   lines = Array.new(rows) { '' }
   each_slice(rows) do |sliced_lists|
     sliced_lists.each_with_index do |list, index|
       lines[index] += list
     end
   end
-  lines
+
+  lines.join("\n") << "\n"
 end
 
-content_name_lists = fetch_target_content_names(CURRENT_DIRECTORY)
+content_name_lists = Dir.entries(CURRENT_DIRECTORY)
 puts print_content_lists(content_name_lists)
