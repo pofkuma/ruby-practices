1847328a7ab8dee76993c63b82901ec8f7cdbf0f
pofkuma <pof.kuma@gmail.com>
2021-11-09 17:08:06 +0900

    Improve some codes
    実装の改善と、テストの修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -5,36 +5,31 @@ CURRENT_DIRECTORY = '.'
 PADDING_SIZE = 7
 MAX_COLUMUNS = 3
 
-public
-
-def print_content_lists(content_name_lists)
-  content_name_lists.reject { |i| /^\./.match?(i) }.sort.format_width.format_lines
-end
-
-# 表示幅を空白詰めで揃える
-def format_width
+def format
   max_name_length = map(&:length).max
-  map { |list| list.ljust(max_name_length + PADDING_SIZE) }
-end
-
-# 折り返して表示する
-def format_lines
-  # 表示行数を求める
   rows = size / MAX_COLUMUNS
-  # ファイル数を列数で割って、割り切れたら商の値、割り切れなかったら商+1の値
   rows += 1 unless (size % MAX_COLUMUNS).zero?
-  return '' if rows.zero?
 
-  # 表示行数で区切って、列数分だけ横にくっつける
   lines = Array.new(rows) { '' }
-  each_slice(rows) do |sliced_lists|
-    sliced_lists.each_with_index do |list, index|
-      lines[index] += list
+  each_slice(rows) do |lists|
+    lists.each_with_index do |list, index|
+      lines[index] += list.ljust(max_name_length + PADDING_SIZE)
     end
   end
 
-  lines.join("\n") << "\n"
+  lines.map(&:rstrip).join("\n") << "\n"
 end
 
+def filter_by_names
+  reject { |name| name.start_with?("\.") }
+end
+
+def list_directory_contents(content_lists)
+  filterd_content_lists = content_lists.filter_by_names
+  filterd_content_lists.sort.format if filterd_content_lists.size.nonzero?
+end
+
+public :format, :filter_by_names, :list_directory_contents
+
 content_name_lists = Dir.entries(CURRENT_DIRECTORY)
-puts print_content_lists(content_name_lists)
+print list_directory_contents(content_name_lists)
--- 05.ls/ls_test.rb
+++ 05.ls/ls_test.rb
@@ -4,57 +4,52 @@ require 'minitest/autorun'
 require_relative 'ls'
 
 class LsTest < Minitest::Test
+  CONTENT_NAMES_WHICH_BEGIN_WITH_A_DOT = %w[. ..].freeze
+
   def test_ls_no_options_no_contents
-    content_name_lists = []
-    expected = ''
-    assert_equal expected, print_content_lists(content_name_lists)
+    content_name_lists = CONTENT_NAMES_WHICH_BEGIN_WITH_A_DOT
+    assert_nil list_directory_contents(content_name_lists)
   end
 
   def test_ls_no_options_one_contents
-    content_name_lists = %w[1]
+    content_name_lists = CONTENT_NAMES_WHICH_BEGIN_WITH_A_DOT + %w[1]
     expected = <<~TEXT
-      1       
+      1
     TEXT
-    assert_equal expected, print_content_lists(content_name_lists)
+    assert_equal expected, list_directory_contents(content_name_lists)
   end
 
   def test_ls_no_options_two_contents
-    content_name_lists = %w[1 2].shuffle
+    content_name_lists = CONTENT_NAMES_WHICH_BEGIN_WITH_A_DOT + %w[1 2].shuffle
     expected = <<~TEXT
-      1       2       
+      1       2
     TEXT
-    assert_equal expected, print_content_lists(content_name_lists)
+    assert_equal expected, list_directory_contents(content_name_lists)
   end
 
   def test_ls_no_options_three_contents
-    content_name_lists = %w[1 2 3].shuffle
+    content_name_lists = CONTENT_NAMES_WHICH_BEGIN_WITH_A_DOT + %w[1 2 3].shuffle
     expected = <<~TEXT
-      1       2       3       
+      1       2       3
     TEXT
-    assert_equal expected, print_content_lists(content_name_lists)
+    assert_equal expected, list_directory_contents(content_name_lists)
   end
 
   def test_ls_no_options_four_contents
-    content_name_lists = %w[1 2 3 4].shuffle
+    content_name_lists = CONTENT_NAMES_WHICH_BEGIN_WITH_A_DOT + %w[1 2 3 4].shuffle
     expected = <<~TEXT
-      1       3       
-      2       4       
+      1       3
+      2       4
     TEXT
-    assert_equal expected, print_content_lists(content_name_lists)
+    assert_equal expected, list_directory_contents(content_name_lists)
   end
 
   def test_ls_no_options_five_contents
-    content_name_lists = %w[1 2 3 4 5].shuffle 
+    content_name_lists = CONTENT_NAMES_WHICH_BEGIN_WITH_A_DOT + %w[1 2 3 4 5].shuffle
     expected = <<~TEXT
-      1       3       5       
-      2       4       
+      1       3       5
+      2       4
     TEXT
-    assert_equal expected, print_content_lists(content_name_lists)
-  end
-
-  def test_ls_no_options_contents_which_have_names_starting_with_dot
-    content_name_lists = %w[. ..].shuffle 
-    expected = ''
-    assert_equal expected, print_content_lists(content_name_lists)
+    assert_equal expected, list_directory_contents(content_name_lists)
   end
 end
