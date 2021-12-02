555b3214074102e9453384dae641dd1ba7e58143
pofkuma <pof.kuma@gmail.com>
2021-12-02 21:08:09 +0900

    Add test for '-l' option
    '-l'オプションのテストケースを追加

--- 05.ls/ls_test.rb
+++ 05.ls/ls_test.rb
@@ -84,4 +84,40 @@ class LsTest < Minitest::Test
     contents = list_directory_contents(options).split("\s")
     assert(contents[-1].start_with?('.'))
   end
+
+  require 'etc'
+  require 'fileutils'
+
+  def test_ls_with_opton_l
+    options = { l: true }
+    contents = list_directory_contents(options).split("\s")
+    assert_includes contents, generate_mock_file_status('test_file')
+    assert_includes contents, generate_mock_file_status('test_dir', is_directory: true)
+  end
+
+  def generate_mock_file_status(pathname, is_directory: false)
+    FileUtils.rm_r("#{__dir__}/test") if File.exist?("#{__dir__}/test")
+    FileUtils.mkdir("#{__dir__}/test")
+    FileUtils.cd("#{__dir__}/test")
+
+    file_type = is_directory ? FileUtils.mkdir(pathname) && 'd' : FileUtils.touch(pathname) && '-'
+    created_time = Time.now
+
+    uid = Etc.getpwuid
+
+    max_length_of_filesize = Dir.open('.').each.map { |file| File.size(file).to_s.length }.max
+    filesize = File.size(pathname)
+
+    {
+      file_type_and_permission: "#{file_type}rw-r--r-- ",
+      number_of_links: '1',
+      owner_name: uid.name.concat("\s"),
+      group_name: Etc.getgrgid(uid.gid).name << "\s",
+      number_of_filesize: filesize.to_s.rjust(max_length_of_filesize),
+      last_modified_date: created_time.strftime('%_2m %_2d %H:%M'),
+      pathname: pathname
+    }.values.join("\s")
+    # example: 'drw-r--r--  1 kumakaori  staff  0 12  2 15:54 test_dir'
+    # example: '-rw-r--r--  1 kumakaori  staff  0 12  2 15:54 test_file'
+  end
 end
