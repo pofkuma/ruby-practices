ce0fa2ee69db9bb3417384a56731de88fc839090
pofkuma <pof.kuma@gmail.com>
2021-12-11 07:11:11 +0900

    Make '-l' option available
    '-l'オプションに対応

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'etc'
+require 'fileutils'
+
 PADDING_SIZE = 7
 MAX_COLUMUNS = 3
 
@@ -14,23 +17,81 @@ def convert_layout(file_names, line_count, max_name_length)
   lines.map(&:rstrip).join("\n")
 end
 
-def format(contents, reverse: false)
-  return if contents.size.zero?
-
-  sorted_contents = contents.sort.then { reverse ? _1.reverse : _1 }
+def format_contents(contents)
   line_count = (contents.size / MAX_COLUMUNS.to_f).ceil
   max_name_length = contents.map(&:length).max
-  convert_layout(sorted_contents, line_count, max_name_length)
+
+  convert_layout(contents, line_count, max_name_length)
 end
 
-def list_directory_contents(options)
-  entries =
-    if options[:a]
-      Dir.glob('*', File::FNM_DOTMATCH)
-    else
-      Dir.glob('*')
-    end
-  format(entries, reverse: options[:r])
+def filetype_char(file_type)
+  case file_type
+  when 'file'             then :-
+  when 'directory'        then :d
+  when 'characterSpecial' then :c
+  when 'blockSpecial'     then :b
+  when 'fifo'             then :p
+  when 'link'             then :l
+  when 'socket'           then :s
+  else raise 'unkown file type'
+  end.to_s
+end
+
+def convert_permission_to_rwx(numbers)
+  numbers.to_i.to_s(2).rjust(3, '0').chars.map.with_index do |flag, index|
+    flag.to_i.zero? ? '-' : %i[r w x][index]
+  end.join
+end
+
+def permissions_string(number)
+  format('%o', number).to_s.chars.map { |char| convert_permission_to_rwx(char) }.join
+end
+
+def query_file_statuses(file)
+  filestatus = File.stat(file)
+  filetype = filetype_char(File.ftype(file))
+  permisions_number = permissions_string(filestatus.world_readable?)
+
+  [[:filetype_and_permissions, "#{filetype}#{permisions_number} "],
+   [:number_of_links,          filestatus.nlink],
+   [:owner_name,               "#{Etc.getpwuid(filestatus.uid).name} "],
+   [:group_name,               "#{Etc.getgrgid(filestatus.gid).name} "],
+   [:number_of_filesize,       filestatus.size],
+   [:last_modified_date,       File.ctime(file).strftime('%_2m %_2d %H:%M')],
+   [:file,                     File.basename(file)]].to_h
+end
+
+def justify_contents_value(contents)
+  justfying_procs =
+    [[:number_of_links,    ->(value, width) { value.rjust(width) }],
+     [:owner_name,         ->(value, width) { value.ljust(width) }],
+     [:group_name,         ->(value, width) { value.ljust(width) }],
+     [:number_of_filesize, ->(value, width) { value.rjust(width) }]].to_h
+
+  justfying_procs.each do |name, proc|
+    max_length = contents.map { _1.fetch(name).to_s.length }.max
+    contents.map { |content| content[name] = proc.call(content.fetch(name).to_s, max_length) }
+  end
+  contents
+end
+
+def format_contents_long(files, base_path)
+  pathnames = files.map { |file| "#{base_path}/#{file}" }
+  total_blocks = pathnames.map { File.stat(_1).blocks }.sum
+  contents = pathnames.map { query_file_statuses(_1) }
+
+  ["total #{total_blocks}"] + justify_contents_value(contents).map { _1.values.join("\s") }
+end
+
+def list_directory_contents(path, all: false, reverse: false, long: false)
+  return "ls: #{path}: No such file or directory" unless FileTest.exist?(path)
+
+  flag = all ? File::FNM_DOTMATCH : nil
+  entries = Dir.glob(*['*', flag].compact, base: path, sort: true)
+               .then { reverse ? _1.reverse : _1 }
+  return if entries.size.zero?
+
+  long ? format_contents_long(entries, path) : format_contents(entries)
 end
 
 if $PROGRAM_NAME == __FILE__
@@ -38,9 +99,12 @@ if $PROGRAM_NAME == __FILE__
   opt = OptionParser.new
 
   options = {}
-  opt.on('-a') { |boolean| options[:a] = boolean }
-  opt.on('-r') { |boolean| options[:r] = boolean }
+  opt.on('-a') { |boolean| options[:all] = boolean }
+  opt.on('-r') { |boolean| options[:reverse] = boolean }
+  opt.on('-l') { |boolean| options[:long] = boolean }
 
   opt.parse!(ARGV)
-  puts list_directory_contents(options)
+
+  path = ARGV[0] || '.'
+  puts list_directory_contents(path, **options)
 end
--- 05.ls/ls_test.rb
+++ 05.ls/ls_test.rb
@@ -1,123 +1,126 @@
 # frozen_string_literal: true
 
 require 'minitest/autorun'
+require 'fileutils'
 require_relative 'ls'
 
 class LsTest < Minitest::Test
-  def test_format_no_contents
-    content_name_lists = []
-    assert_nil format(content_name_lists)
+  TEST_DIR = "#{__dir__}/test"
+
+  def setup
+    FileUtils.rm_r(TEST_DIR) if File.exist?(TEST_DIR)
+    FileUtils.mkdir(TEST_DIR)
   end
 
-  def test_format_one_contents
-    content_name_lists = %w[1]
+  def teardown
+    FileUtils.rm_r(TEST_DIR) if File.exist?(TEST_DIR)
+  end
+
+  def test_ls_without_opton_no_contents
+    assert_nil list_directory_contents(TEST_DIR)
+  end
+
+  def test_ls_without_opton_one_contents
+    %w[1].each { FileUtils.touch("#{TEST_DIR}/#{_1}") }
     expected = <<~TEXT.chomp
       1
     TEXT
-    assert_equal expected, format(content_name_lists)
+    assert_equal expected, list_directory_contents(TEST_DIR)
   end
 
-  def test_format_two_contents
-    content_name_lists = %w[1 2].shuffle
+  def test_ls_without_opton_two_contents
+    %w[1 2].each { FileUtils.touch("#{TEST_DIR}/#{_1}") }
     expected = <<~TEXT.chomp
       1       2
     TEXT
-    assert_equal expected, format(content_name_lists)
+    assert_equal expected, list_directory_contents(TEST_DIR)
   end
 
-  def test_format_three_contents
-    content_name_lists = %w[1 2 3].shuffle
+  def test_ls_without_opton_three_contents
+    %w[1 2 3].each { FileUtils.touch("#{TEST_DIR}/#{_1}") }
     expected = <<~TEXT.chomp
       1       2       3
     TEXT
-    assert_equal expected, format(content_name_lists)
+    assert_equal expected, list_directory_contents(TEST_DIR)
   end
 
-  def test_format_four_contents
-    content_name_lists = %w[1 2 3 4].shuffle
+  def test_ls_without_opton_four_contents
+    %w[1 2 3 4].each { FileUtils.touch("#{TEST_DIR}/#{_1}") }
     expected = <<~TEXT.chomp
       1       3
       2       4
     TEXT
-    assert_equal expected, format(content_name_lists)
+    assert_equal expected, list_directory_contents(TEST_DIR)
   end
 
-  def test_format_five_contents
-    content_name_lists = %w[1 2 3 4 5].shuffle
+  def test_ls_without_opton_five_contents
+    %w[1 2 3 4 5].each { FileUtils.touch("#{TEST_DIR}/#{_1}") }
     expected = <<~TEXT.chomp
       1       3       5
       2       4
     TEXT
-    assert_equal expected, format(content_name_lists)
-  end
-
-  def test_format_five_contents_reverse
-    content_name_lists = %w[1 2 3 4 5].shuffle
-    expected = <<~TEXT.chomp
-      5       3       1
-      4       2
-    TEXT
-    assert_equal expected, format(content_name_lists, reverse: true)
+    assert_equal expected, list_directory_contents(TEST_DIR)
   end
 
   def test_ls_without_opton_a
     options = {}
-    contents = list_directory_contents(options).split("\s")
-    assert_nil(contents.find { |content| content.start_with?('.') })
+    assert_nil list_directory_contents(TEST_DIR, **options)
   end
 
   def test_ls_with_opton_a
-    options = { a: true }
-    contents = list_directory_contents(options).split("\s")
+    options = { all: true }
+    contents = list_directory_contents(TEST_DIR, **options).split("\s")
     assert(contents.find { |content| content.start_with?('.') })
     assert(contents.find { |content| content.start_with?('..') })
   end
 
   def test_ls_without_opton_r
-    options = { a: true }
-    contents = list_directory_contents(options).split("\s")
+    options = { all: true }
+    contents = list_directory_contents(TEST_DIR, **options).split("\s")
     assert(contents[0].start_with?('.'))
   end
 
   def test_ls_with_opton_r
-    options = { a: true, r: true }
-    contents = list_directory_contents(options).split("\s")
+    options = { all: true, reverse: true }
+    contents = list_directory_contents(TEST_DIR, **options).split("\s")
     assert(contents[-1].start_with?('.'))
   end
 
-  require 'etc'
-  require 'fileutils'
-
-  def test_ls_with_opton_l
-    options = { l: true }
-    contents = list_directory_contents(options).split("\s")
-    assert_includes contents, generate_mock_file_status('test_file')
-    assert_includes contents, generate_mock_file_status('test_dir', is_directory: true)
+  def test_ls_without_opton_l
+    options = { all: true }
+    contents = list_directory_contents(TEST_DIR, **options)
+    assert(contents[0].start_with?('.'))
   end
 
-  def generate_mock_file_status(pathname, is_directory: false)
-    FileUtils.rm_r("#{__dir__}/test") if File.exist?("#{__dir__}/test")
-    FileUtils.mkdir("#{__dir__}/test")
-    FileUtils.cd("#{__dir__}/test")
+  def test_ls_with_opton_l
+    FileUtils.mkdir("#{TEST_DIR}/dummy_dir")
+    FileUtils.touch("#{TEST_DIR}/dummy_file")
+    expected_contents = `ls -l #{TEST_DIR}`.split("\n")
 
-    file_type = is_directory ? FileUtils.mkdir(pathname) && 'd' : FileUtils.touch(pathname) && '-'
-    created_time = Time.now
+    options = { long: true }
+    contents = list_directory_contents(TEST_DIR, **options)
+    assert_equal expected_contents, contents
+  end
 
-    uid = Etc.getpwuid
+  def test_filetype_char
+    files =
+      # Paths are on macOS
+      { '-': '/var/log/system.log',                            # normal file
+        'd': '/dev',                                           # directory
+        'c': '/dev/null',                                      # characterSpecial file
+        'b': '/dev/disk0',                                     # blockSpecial file
+        'p': "#{TEST_DIR}/dummy_fifo".tap { File.mkfifo(_1) }, # fifo file
+        'l': '/var',                                           # link file
+        's': '/var/run/syslog' }                               # socket file
 
-    max_length_of_filesize = Dir.open('.').each.map { |file| File.size(file).to_s.length }.max
-    filesize = File.size(pathname)
+    files.each do |char, file|
+      filetype = File.ftype(file)
+      assert_equal char.to_s, filetype_char(filetype)
+    end
+  end
 
-    {
-      file_type_and_permission: "#{file_type}rw-r--r-- ",
-      number_of_links: '1',
-      owner_name: uid.name.concat("\s"),
-      group_name: Etc.getgrgid(uid.gid).name << "\s",
-      number_of_filesize: filesize.to_s.rjust(max_length_of_filesize),
-      last_modified_date: created_time.strftime('%_2m %_2d %H:%M'),
-      pathname: pathname
-    }.values.join("\s")
-    # example: 'drw-r--r--  1 kumakaori  staff  0 12  2 15:54 test_dir'
-    # example: '-rw-r--r--  1 kumakaori  staff  0 12  2 15:54 test_file'
+  def test_filetype_char_error
+    error = assert_raises(RuntimeError) { filetype_char(nil) }
+    assert_equal 'unkown file type', error.message
   end
 end
